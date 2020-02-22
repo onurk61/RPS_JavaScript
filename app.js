69481b38f0224800fda0ffa1bb21cfedd3d0859e
onur <onurkanca3@gmail.com>
2020-02-22 14:16:48 +0300

    "updated"

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const getPlayerChoice = () => {
         selection !== SCISSORS
     ) {
         alert(`Invalid choice! We choose ${DEFAULT_USER_CHOICE} for you!`);
-        return DEFAULT_USER_CHOICE;
+        return;
     }
     return selection;
 };
@@ -35,7 +35,7 @@ const getComputerChoice = () => {
     }
 };
 
-const getWinner = (cChoice, pChoice) => 
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) => 
      cChoice === pChoice 
     ? RESULT_DRAW 
     : (cChoice === ROCK && pChoice === PAPER) ||
@@ -61,6 +61,59 @@ startGameBtn.addEventListener('click', () => {
     console.log('Game is starting...');
     const playerChoice = getPlayerChoice();
     const computerChoice = getComputerChoice();
-    const winner = getWinner(computerChoice,playerChoice);
-    console.log(winner);
+    let winner;
+    if(playerChoice){
+        winner = getWinner(computerChoice,playerChoice);
+    }else {
+        winner = getWinner(computerChoice);
+    }
+    let message = `You picked ${playerChoice || DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefore you `;
+    if(winner === RESULT_DRAW){
+        message = message + 'had a draw.';
+    }else if (winner === RESULT_PLAYER_WINS) {
+        message = message + 'won.';
+    } else {
+        message = message + 'lost.';
+    }
+    alert(message);
+    gameIsRunning = false;
 });
+
+// // not related to game
+// // ... represents rest operator, and it should be at the end of the parameters' list
+// // you cannot have more than one rest operator (...)  
+// const combine = (resultHandler,operation, ...numbers) => {    
+//     const validateNumber = (number) => {
+//         return isNaN(number) ? 0 : number;
+//     };
+// let sum= 0;
+// for (const num of numbers){
+//     if(operation === 'ADD'){
+//         sum += validateNumber(num);
+//     }
+//     else {
+//         sum -= validateNumber(num);
+//     }
+// }
+// resultHandler(sum);
+// };
+
+// //if you use function key word, you will have permission to use arguments instead of rest operator(...) and numbers[]
+
+// // const subtractUp = function (resultHandler, ...numbers) {
+// //     let sum= 0;
+// // for (const num of numbers){ // don't use that
+// //     sum -= num;
+// // }
+// // resultHandler(sum, 'The result after adding all numbers is');
+// // };
+
+// const showResult = (messageText, result) => {
+//     alert(messageText +' ' + result);
+// };
+
+// //console.log(sumUp([1,5,10,-3,6,10,25,88]));
+// // console.log(sumUp(1,5,10,-3,6,10,25,88));
+// // subtractUp(showResult,1,5,10,-3,6,10,25,88);
+// combine(showResult.bind(this,'The result after adding all numbers is:'),'ADD',1,5,10,-3,6,10,25,88);
+// combine(showResult.bind(this, 'The result after subtracting all numbers is:'), 'SUBTRACT', 10, -3, 6, 10, 25, 88);
